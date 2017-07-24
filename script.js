f96c68327e0da5fb3e1d8b86c376fa3a126c931b
Anjali vijay <anjali.vijay@qed42.com>
2017-07-24 18:46:33 +0530

    Manual validations done.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 (function($){
   $(document).ready(function(){
 
-    var email = /^[A-Za-z0-9._]*\@[A-Za-z]*\.[A-Za-z]{2,5}$/;
-    var name = $("#name").val();
-    var lname = $("#lname").val();
-    var password = $("#password").val();
-    var repassword = $("#repassword").val();
-    var passport = $("#passport").val();
-    var date = $("#date").val();
-    var tags = $("#tags").val();
-    var valid = true;
-
     $("input[type='radio']").change(function() {
       if ($(this).val() == "first") {
         $(".list").show();
@@ -28,23 +18,73 @@
     });
 
     $('.form-content input').blur(function(){
-      printError($(this).parent(),$(this).data('errormes'));
+      if(emptyValidation($(this).parent(),$(this).data('errormes'))){
+        //exit the code
+        return;
+      }
+      if(($(this).attr("id")=='fname' || $(this).attr("id")=='lname') && $(this).val().trim().length < 3) {
+        printError($(this).parent(),$(this).data('type'));
+      }
+      else {
+        removeError($(this).parent());
+      }
+      if($(this).attr("id")=='email'){
+        var patt=/^[A-Za-z0-9._]*\@[A-Za-z0-9._]*\.[A-Za-z]{2,5}$/;
+        var mail=$('#email').val();
+        var res=patt.test(mail);
+        if(!(res)){
+          printError($(this).parent(),$(this).data('type'));
+        }
+				else{
+        removeError($(this).parent());
+      	}
+    	}
+      if($(this).attr("id")=='password'){
+      	 pswd=$(this).val();
+        if(!(pswd.length > 8 && pswd.match(/[a-z]/) && pswd.match(/[A-Z]/) && pswd.match(/\d/)))
+        {
+					printError($(this).parent(),$(this).data('type'));
+        }
+        else{
+          removeError($(this).parent());
+        }
+      }
+      if($(this).attr("id")=='repassword'){
+      	var repswd=$(this).val();
+        if(!(repswd==pswd))
+        {
+					printError($(this).parent(),$(this).data('type'));
+        }
+        else{
+          removeError($(this).parent());
+        }
+      }
     });
   });
 
   function printError($el,mes){
     $errordiv = $( "<div class='alert-error' id='errorContainer'>" + mes + "</div>" );
-    if($el.find('input').val().length < 1){
-      $el.closest('.form-content').find('label').addClass('error');
-      $el.find('input').addClass('error');
-      if($el.find('.alert-error').length == 0){
-        $el.append($errordiv);
-      }
+    $el.closest('.form-content').find('label').addClass('error');
+    $el.find('input').addClass('error');
+    if($el.find('.alert-error').length == 0){
+      $el.append($errordiv);
+    }
+  }
+
+  function removeError($el){
+    $el.closest('.form-content').find('label').removeClass('error');
+    $el.find('input').removeClass('error');
+    $el.find('.alert-error').remove();
+  }
+
+  function emptyValidation($el,mes){
+    if($el.find('input').val().trim(" ").length < 1){
+      printError($el,mes);
+      return true;
     }
     else{
-      $el.closest('.form-content').find('label').removeClass('error');
-      $el.find('input').removeClass('error');
-      $el.find('.alert-error').remove();
+      removeError($el);
+      return false;
     }
   }
 
